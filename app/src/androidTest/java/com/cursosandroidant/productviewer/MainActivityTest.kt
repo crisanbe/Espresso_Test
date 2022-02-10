502042c372b34e803cf4660cdafa066119d373d1
crisanbe <55001756+crisanbe@users.noreply.github.com>
2022-02-10 11:12:46 -0500

    (Seccion 6:Espresso):Más funciones con Espresso. fun limitar el incremento en campo de texto por medio del evento onclick

--- app/src/androidTest/java/com/cursosandroidant/productviewer/MainActivityTest.kt
+++ app/src/androidTest/java/com/cursosandroidant/productviewer/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.cursosandroidant.productviewer
 
 
+import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -16,12 +17,12 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest//TODO- (@LargeTest) indicarle que la prueba sera grande, y puede aplicarse a nivel de clase o metodo
-class MainActivityTest{
+class MainActivityTest {
     @get:Rule//TODO - regla
     val activityRule = ActivityScenarioRule(MainActivity::class.java)//TODO- escenario
 
     @Test// funcion incrementar en campo de texto
-    fun setNewQuantity_sum_increasesTexField(){
+    fun setNewQuantity_sum_increasesTexField() {
         onView(withId(R.id.etNewQuantity))//campo texField
             .check(matches(withText("1")))//TODO- nos posicionamos en el estado inicial 1
 
@@ -32,4 +33,26 @@ class MainActivityTest{
             .check(matches(withText("2")))//TODO- nos posicionamos en 2
 
     }
+
+    @Test//TODO- funcion incrementar limitado en campo de texto
+    fun setNewQuantity_sumLimit_noIncreasesTexField() {
+        val scenario = activityRule.scenario
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.onActivity { activity ->
+            activity.selectedProduct.quantity =
+                1//TODO- limitando que tanto para incrementar como decrementar va ser imposible
+        }
+
+        onView(withId(R.id.etNewQuantity))//campo texField
+            .check(matches(withText("1")))//TODO- nos posicionamos en el estado inicial 1
+
+        onView(withId(R.id.ibSum))//campo incrementar o sumar
+            .perform(click())//performa para desirle que es un evento onclick
+
+        onView(withId(R.id.etNewQuantity))//TODO- le indicamo que al dar click nuestro texField pase a la posicion 2
+            .check(matches(withText("1")))//TODO- nos posicionamos en 1
+
+
+    }
 }
+
